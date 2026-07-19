95837d3f2d55a7b0b4bc9f02f6b887289939d1e4
cskrisztian <csipkekrisztian@gmail.com>
2026-07-19 14:02:36 +0200

    Feature - changing humble cron job to 19:00

--- cronjobs/cron-humble.js
+++ cronjobs/cron-humble.js
@@ -3,7 +3,7 @@ import * as humble from '../commands/humble.js';
 
 export function setCronjob(client){
     console.log("Humble bundle choice cron job initiation")
-    cron.schedule('0 9 1-7 * 2', async () => {
+    cron.schedule('0 19 1-7 * 2', async () => {
         console.log("Sending out Humble Choice alert!")
         await humble.sendChoiceAlert(client);
     }, {
