371b3c28109b82ad83270f15cc251dd3405c9253
cskrisztian <csipkekrisztian@gmail.com>
2026-09-10 15:59:04 +0200

    Fix - test command fix

--- commands/test-commands.js
+++ commands/test-commands.js
@@ -1,9 +1,14 @@
 ﻿import * as humble from "./humble.js";
+import * as messagingController from "../controllers/messaging-controller.js";
 
 export async function humbleTest(interaction){
     const testType = interaction.options.getString("type");
     console.log("Test command fired with type: " + testType + "...")
     if (testType === "humble") {
         await humble.sendChoiceAlert(interaction.client);
+        await messagingController.sendMsgToAdmin(interaction, {
+            content: "✅ Humble Choice értesítés sikeresen kiküldve!",
+            ephemeral: true
+        });
     }
 }
